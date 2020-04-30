be2efbf63d84131301cbc599623d251b58dea71b
PhoenixTAN <tanziqi1756@outlook.com>
2020-04-30 16:31:10 -0400

    Parpare and update the thread switch.

---
+++ Ziqi-FileSystem-v2/heap.h
@@ -0,0 +1,14 @@
+/**
+ * This file refers to the code from Qijia Liu.
+*/
+#ifndef HEAP_H
+#define HEAP_H
+
+#define NULL 0
+typedef unsigned size_t;
+
+void init_heap(void);
+void *malloc(size_t);
+void free(void *);
+
+#endif
