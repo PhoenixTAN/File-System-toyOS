148db7ab0cb901233d8a68e943da645b0787b962
PhoenixTAN <tanziqi1756@outlook.com>
2020-05-02 15:25:22 -0400

    Testing.

--- Ziqi-Ramdisk/userspace/filesysUtil_temp.c
+++ Ziqi-Ramdisk/userspace/filesysUtil_temp.c
@@ -1,4 +1,5 @@
-#include "discos.h"
+#include <stdio.h>
+#include <string.h>
 
 /*
 在根目录创建文件，找到根目录inode，根目录inode直接指针指向一个free_block，
@@ -12,9 +13,9 @@ cd
 cd ..
 */
 
+int get_free_block_from_bitmap(char* map);
 
-int main ()
-{
+int main () {
     char str[] ="- This, a sample string.";
     char * pch;
     printf ("Splitting string \"%s\" into tokens:\n",str);
@@ -24,12 +25,21 @@ int main ()
         printf ("%s\n",pch);
         pch = strtok (NULL, " ,.-");
     }
+
+    char bitmap[1024];
+    int i;
+    int pos;
+    printf("bitmap size: %d\n", sizeof(bitmap));
+    // output 0
+    for ( i = 0; i < 1024; i++ ) {
+        bitmap[i] = 255;
+    }
+    bitmap[0] = 254;
+    pos = get_free_block_num_from_bitmap(bitmap);
+    printf("pos: %d\n", pos);
+
     return 0;
 }
-
-
-data_block_struct* get_free_block_from_bitmap(char* map, filesys_struct* filesys);
-char* get_pwd();
 
 /* get the free block number from bitmap 
     return the number of the free data block.
@@ -38,13 +48,13 @@ char* get_pwd();
 int get_free_block_num_from_bitmap(char* map) {
     // map: char bitmap[BITMAP_SIZE*BLOCK_SIZE]; 8192 bits in all
     // the first 7931 blocks are usable
-
+    printf("map length: %d\n", strlen(map));
     // find the first 0 bit in bit map
     int i;
     int free_byte_pos;
     char free_byte;
     // iterate bit map byte by byte
-    for ( i = 0; i < BITMAP_SIZE*BLOCK_SIZE; i++ ) {
+    for ( i = 0; i < /*BITMAP_SIZE*BLOCK_SIZE*/1024; i++ ) {
         char ch = map[i];
         if ( ch != 255 )  {
             free_byte_pos = i;
@@ -54,15 +64,16 @@ int get_free_block_num_from_bitmap(char* map) {
     }
 
     // no free data block
-    if ( i == BITMAP_SIZE*BLOCK_SIZE ) {
+    if ( i == /*BITMAP_SIZE*BLOCK_SIZE*/1024 ) {
         return -1;
     }
-
+    printf("byte i=%d", i);
     // iterate a byte bit by bit
     int free_bit;
     for ( i = 0, free_bit = free_byte_pos * 8; i < 8; i++ ) {
         if ( (free_byte & 1 ) == 0 ) {
             free_bit += i;
+            free_byte = free_byte >> 1;
             break;
         }
     }
